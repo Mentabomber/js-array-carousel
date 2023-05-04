167e7f909121c3ca3ef3773b8ccafa14da835dc6
Menta <simcictilen.gmail.com>
2023-05-04 20:45:42 +0200

    buttons working

--- js/main.js
+++ js/main.js
@@ -1,41 +1,70 @@
 // Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
 
 const imageList = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
-let createImg, createDiv;
+
+let indexImmagineAttiva = 0;
+
+let containerSelector = document.getElementById("container");
+console.log(containerSelector)
 
 
 
+for(let i = 0; i <= imageList.length - 1; i++){
 
-
-for(let i = 0; i <= (imageList.length -1); i++){
-
-    let containerSelector = document.getElementById("container");
-
-    createDiv = document.createElement('div');
-
-    createDiv.classList.add("img-container", "fresh");
-    
-    containerSelector.append(createDiv);
-    
-    containerSelector = document.getElementsByClassName("img-container fresh"); 
-    
-    createImg.innerHtml = `<img src= $(imageList) alt="static-test-img">`;
-
-    // createImg = document.createElement('img');
-
-    // createImg.classList.add("myImg");
-
-    // containerSelector.append(createImg);
-
- 
-    
-    imageList[i];
-
-    // createImg = document.classList.remove("fresh");
-
-    // createImg.append(imageList)
-   console.log(imageList);
+    containerSelector.innerHTML += `<div class="img-container"> 
+                                        <img src=${imageList[i]} alt="static-test-img">
+                                    </div>` 
 
 }
 
-console.log(containerSelector);
+let container = document.getElementById("container");
+let immagini = container.querySelectorAll("div");
+immagini[indexImmagineAttiva].classList.add("active");
+
+let  buttonInteractionUp = document.getElementById("arrow-up-carousel");
+buttonInteractionUp.addEventListener('click',
+    function(){
+
+        immagini[indexImmagineAttiva].classList.remove("active");
+        indexImmagineAttiva++;
+        if(indexImmagineAttiva == imageList.length ){
+            indexImmagineAttiva = 0;
+
+        }
+        immagini[indexImmagineAttiva].classList.add("active");
+        console.log(indexImmagineAttiva);
+        
+
+    
+
+    }
+);
+let  buttonInteractionDown = document.getElementById("arrow-down-carousel");
+buttonInteractionDown.addEventListener('click',
+    function(){
+
+        immagini[indexImmagineAttiva].classList.remove("active");
+        if(indexImmagineAttiva == 0 ){
+            indexImmagineAttiva = imageList.length;
+
+        }
+        indexImmagineAttiva--;
+        immagini[indexImmagineAttiva].classList.add("active");
+        console.log(indexImmagineAttiva);
+        
+
+    
+
+    }
+);
+
+
+
+
+
+console.log(buttonInteraction);
+
+
+
+
+// console.log(containerSelector);
