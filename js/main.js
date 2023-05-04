4b8cc2df64874e7bb4ba9035ac9da0c34f5111fe
Menta <simcictilen.gmail.com>
2023-05-04 17:25:39 +0200

    update creazione div  e  img dentro html da js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 // Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
 
 const imageList = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
-let createDiv, createImg;
-
-let containerSelector = document.getElementsByClassName("container");
+let createImg, createDiv;
 
 
 
 
 
 for(let i = 0; i <= (imageList.length -1); i++){
-    
-    createDiv = document.createElement("div").classList.add("img-container" + "fresh");
 
-    createImg = document.getElementsByClassName("fresh"); 
-    .innerHtml(`<img src=` + imageList + ` alt="static-test-img">`)
-    createImg.append(imageList)
-    imageList++;
+    let containerSelector = document.getElementById("container");
+
+    createDiv = document.createElement('div');
+
+    createDiv.classList.add("img-container", "fresh");
+    
+    containerSelector.append(createDiv);
+    
+    containerSelector = document.getElementsByClassName("img-container fresh"); 
+    
+    createImg.innerHtml = `<img src= $(imageList) alt="static-test-img">`;
+
+    // createImg = document.createElement('img');
+
+    // createImg.classList.add("myImg");
+
+    // containerSelector.append(createImg);
+
+ 
+    
+    imageList[i];
+
+    // createImg = document.classList.remove("fresh");
+
+    // createImg.append(imageList)
+   console.log(imageList);
 
 }
 
